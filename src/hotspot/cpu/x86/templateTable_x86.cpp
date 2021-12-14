c02db19926872f0a83ee0c523ec70405208e461f
Dat Nguyen <tiendat.ng.cs@gmail.com>
2021-12-13 18:55:01 -0600

    can only use call_VM once?

--- src/hotspot/cpu/x86/templateTable_x86.cpp
+++ src/hotspot/cpu/x86/templateTable_x86.cpp
@@ -204,17 +204,17 @@ static void do_oop_load(InterpreterMacroAssembler* _masm,
   __ load_heap_oop(dst, src, rdx, rbx, decorators);
   if (barrier == BarrierSet::ShenandoahBarrierSet){
     if (!is_array){
-      // __ push_ptr(rax);
-      __ pusha();
+      __ push_ptr(rax);
+      // __ pusha();
       __ verify_oop(dst);
       __ call_VM(noreg, CAST_FROM_FN_PTR(address, InterpreterRuntime::read_barrier), dst);
-      // __ pop_ptr(rax);
-      __ popa();
+      __ pop_ptr(rax);
+      // __ popa();
     }
   }
-  if (barrier == BarrierSet::ShenandoahBarrierSet) {
-    __ call_VM(noreg, CAST_FROM_FN_PTR(address, InterpreterRuntime::print_something));
-  }
+  // if (barrier == BarrierSet::ShenandoahBarrierSet) {
+  //   __ call_VM(noreg, CAST_FROM_FN_PTR(address, InterpreterRuntime::print_something));
+  // }
 }
 
 Address TemplateTable::at_bcp(int offset) {
