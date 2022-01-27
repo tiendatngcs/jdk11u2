5344fe92580312937fdacc6b3b1c54076ab62105
Dat Nguyen <tiendat.ng.cs@gmail.com>
2022-01-26 22:16:14 -0600

    update ac with asm

--- src/hotspot/cpu/x86/gc/shenandoah/shenandoahBarrierSetAssembler_x86.cpp
+++ src/hotspot/cpu/x86/gc/shenandoah/shenandoahBarrierSetAssembler_x86.cpp
@@ -347,10 +347,10 @@ void ShenandoahBarrierSetAssembler::shenandoah_write_barrier_post(MacroAssembler
   __ call_VM_leaf(CAST_FROM_FN_PTR(address, ShenandoahRuntime::print_oop), obj);
   __ popa();
 
-  // assert_different_registers(obj, r9);
+  assert_different_registers(obj, r9);
 
   // load ac value to r9
-  // __ load_sized_value(r9, Address(obj, oopDesc::access_counter_offset_in_bytes()), sizeof(intptr_t), false);
+  __ load_sized_value(r9, Address(obj, oopDesc::access_counter_offset_in_bytes()), sizeof(intptr_t), false);
 
 
   __ pusha();
@@ -358,7 +358,7 @@ void ShenandoahBarrierSetAssembler::shenandoah_write_barrier_post(MacroAssembler
   __ popa();
 
   // increment the ac value
-  // __ increment(r9);
+  __ increment(r9);
 
   __ pusha();
   __ call_VM_leaf(CAST_FROM_FN_PTR(address, ShenandoahRuntime::print_oop), obj);
@@ -368,12 +368,12 @@ void ShenandoahBarrierSetAssembler::shenandoah_write_barrier_post(MacroAssembler
 
 
   // void store_sized_value(Address dst, Register src, size_t size_in_bytes, Register src2 = noreg);
-  // __ store_sized_value(Address(obj, oopDesc::access_counter_offset_in_bytes()), r9, sizeof(intptr_t));
+  __ store_sized_value(Address(obj, oopDesc::access_counter_offset_in_bytes()), r9, sizeof(intptr_t));
 
   // __ movptr(r8, obj);
-  __ pusha();
-  __ call_VM_leaf(CAST_FROM_FN_PTR(address, ShenandoahRuntime::write_barrier_helper), obj);
-  __ popa();
+  // __ pusha();
+  // __ call_VM_leaf(CAST_FROM_FN_PTR(address, ShenandoahRuntime::write_barrier_helper), obj);
+  // __ popa();
   
   // __ pop(r9);
 
