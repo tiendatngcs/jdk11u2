ece6cd10b1b3ea913af47753f0d41e6f588dd2c6
Dat Nguyen <tiendat.ng.cs@gmail.com>
2022-02-02 16:15:46 -0600

    update increase_access_counter

--- src/hotspot/share/oops/oop.hpp
+++ src/hotspot/share/oops/oop.hpp
@@ -75,7 +75,7 @@ class oopDesc {
   inline markOop* mark_addr_raw() const;
 
   inline void set_access_counter(uintptr_t new_value);
-  inline void increase_access_counter(uintptr_t increment);
+  inline void increase_access_counter();
 
   inline void set_gc_epoch(uintptr_t new_value);
   inline void add_gc_epoch(uintptr_t increment);
